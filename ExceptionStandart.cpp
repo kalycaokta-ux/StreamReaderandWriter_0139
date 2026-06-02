d5cc22678c2cfe3defc895b285275cf16b89ea5b
kalycaokta-ux <kalycaokta@gmail.com>
2026-06-02 17:40:44 +0700

    menambahkan deklrasi array element ke-5

--- ExceptionStandart.cpp
+++ ExceptionStandart.cpp
@@ -12,7 +12,8 @@ int main()
     {
         //deklrasi array data dengan panjang index 3
         array <int, 3> data = { 1, 2, 3 };
-        //memanggil array element ke-5
+        //memanggil array element ke-5 yang berarti akan terjadi error
+        cout << data.at(5) << endl;
         
     }
     
