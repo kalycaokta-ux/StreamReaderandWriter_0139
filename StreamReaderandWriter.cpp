c4b582338ff5242fc66426091979d2c99580977b
kalycaokta-ux <kalycaokta@gmail.com>
2026-06-02 18:44:28 +0700

    menambahkan outfile.close sebagai penulisan selesai

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -25,5 +25,10 @@ int main()
         getline(cin, baris);
         //kondisi false jika baris hanya berisi karakter q
         if (baris == "q") break;
+        //menulis dan memasukkan nilai dari variabel baris ke dalam file
+        outfile << baris << endl;
+
     }
-}
+    //menutup file setelah penulisan selesai
+    outfile.close();
+}   
