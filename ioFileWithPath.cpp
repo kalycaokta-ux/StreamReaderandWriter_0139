68272270a5ee3555aa7d8799e6244306b0ae5717
kalycaokta-ux <kalycaokta@gmail.com>
2026-06-02 18:09:01 +0700

    menambahkan kondisi jika file ada

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -35,4 +35,14 @@ int main()
     }
     //menutup file apabila telah selesai ditulis
     outfile.close();
+
+    //membuka file dalam mode membaca 
+    ifstream infile;
+
+    //menunjuk ke sebuah file dengan ekstensi .txt
+    infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">= Membuka dan membaca file" << endl;
+    //kondisi jika file ada
+    if (infile.is_open())
 }
