65bb2d2ef370659fd8d5ebd401df4ec0dc8b9dd4
kalycaokta-ux <kalycaokta@gmail.com>
2026-06-02 17:52:59 +0700

    menambahkan  outfile open

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -8,4 +8,13 @@ int main()
 
     //deklarasi variabel baris dan NamaFile
     string baris, NamaFile;
+
+    //membuat input untuk menangkap nama file
+    cout << "Masukkan nama file :";
+    cin >> NamaFile;
+
+    //mmebuat file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file dengan ekstentsi txt
+    outfile.open(NamaFile + ".txt", ios::out);
 }
