01deb464e821d40bb1dd6cc88728f43ad7b49e88
kalycaokta-ux <kalycaokta@gmail.com>
2026-06-02 17:39:24 +0700

    menambahkan penanda awal program

--- ExceptionStandart.cpp
+++ ExceptionStandart.cpp
@@ -2,3 +2,18 @@
 #include <exception>
 #include <array>
 using namespace std;
+
+
+int main()
+{
+
+    cout << "Awal program" << endl; //penanda 1
+    try
+    {
+        //deklrasi array data dengan panjang index 3
+        array <int, 3> data = { 1, 2, 3 };
+        //memanggil array element ke-5
+        
+    }
+    
+}
