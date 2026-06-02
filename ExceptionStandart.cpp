ae60c75bbb6a70f8243cd9900ea9c13769aaa430
kalycaokta-ux <kalycaokta@gmail.com>
2026-06-02 17:43:12 +0700

    menambahkan penanda 2 baris program terakhir

--- ExceptionStandart.cpp
+++ ExceptionStandart.cpp
@@ -14,7 +14,14 @@ int main()
         array <int, 3> data = { 1, 2, 3 };
         //memanggil array element ke-5 yang berarti akan terjadi error
         cout << data.at(5) << endl;
-        
     }
-    
+    //konidisi jika terjadi error di dalam try
+    catch (exception& e)
+    {
+        
+        cout << e.what() << endl;
+    }
+    cout << "Baris program terakhir" << endl; //penanda 2
+    return 0;
+
 }
